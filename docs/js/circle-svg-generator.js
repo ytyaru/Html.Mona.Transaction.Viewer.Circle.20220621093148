cfb850a183f34100cea7b3da9d85e6ea5792bde8
ytyaru <yttry0@gmail.com>
2022-06-28 21:02:38 +0900

    画像サイズから重ならない最大配置数をハードコーディングした。

--- docs/js/circle-svg-generator.js
+++ docs/js/circle-svg-generator.js
@@ -3,17 +3,15 @@ class CircleSvgGenerator {
         this.profiles = profiles
         console.debug(myAddress, this.profiles)
         this.my = this.#makeUserData(myAddress)
-        //this.my = this.profiles.find(p=>p.address==myAddress)
-        //if (!this.my) { this.my = {address:myAddress, name:null, url:null, avatar:'./asset/image/user/kkrn_icon_user_3_resize_min.svg'} }
         this.userIconSizes = [96,72,64,48,32,24,20] // 差24,8,16,16,8,4
-        this.userIconNum = [1,8,16,32,48,64,88]
-        //this.size = this.userIconSizes[0] + this.userIconSizes.slice(1).reduce((sum,v)=>sum+v)*2
+        //this.userIconNum = [1,8,16,32,48,64,88]
+        this.userIconNum = [1, 7, 14, 27, 48, 72, 93]
     }
     generate(partners) { // partners: 取引相手のアドレスとプロフィール情報の配列
-    //generate() { // 中央ユーザの表示サイズ、周辺ユーザの表示数＆表示サイズ
         this.partners = partners
         console.debug(this.partners)
-        // 外周に並べる画像数の合計[8, 24, 56, 104, 168, 256]
+        console.debug(this.userIconNum)
+        // 外周に並べる画像数の合計[8, 24, 56, 104, 168, 256] [7, 21, 48, 96, 168, 261]
         this.outerNums = this.userIconNum.slice(1).map((v,i)=>this.userIconNum.slice(1,i+2).reduce((sum,v)=>sum+v))
         console.debug(this.outerNums)
         const find = this.outerNums.findIndex(sum=>this.partners.length < sum)
@@ -23,8 +21,11 @@ class CircleSvgGenerator {
         this.outerLen = idx+1
         console.debug(this.outerLen)
         // 画像全体サイズ
-        this.size = this.userIconSizes[0] + ((this.outerLen < 1) ? 0 : this.userIconSizes.slice(1,this.outerLen+1).reduce((sum,v)=>sum+v)*2)
+        this.size = (this.userIconSizes.slice(0,this.outerLen+1).reduce((sum,v)=>sum+v)*2) - this.userIconSizes[0]
         console.debug(this.size)
+        // 外周ごとの画像数
+        //this.userIconNum = Array.from({length: this.outerLen}, (v, k) => k).map(i=>this.#calcIconNum(i))
+        //console.debug(this.userIconNum)
         return this.#makeSvg(this.#makeCss()+this.#makeBackground()+this.#makeUsers())
     }
     #makeSvg(content) { return `<svg width="${this.size}" height="${this.size}">${content}</svg>` }
@@ -32,20 +33,6 @@ class CircleSvgGenerator {
     #makeClipCss() { return this.userIconSizes.map(size=>`.clip-${size}{ clip-path: circle(${size/2}px at center); }`).join('\n') }
     #makeBackground() { return `<rect x="0" y="0" width="${this.size}" height="${this.size}" style="fill:green" />` }
     #makeUsers() {
-        //this.userIconSizes.slice(1).reduce((sum,v)=>sum+v)
-        // 自分より前までの値を加算した配列
-        // [8,16,32,48,64,88]
-        // [8,24,56,104,168,256]
-        //const find = this.userIconNum.slice(1).findIndex(n=>n < partners.length)
-        /*
-        const find = this.outerNums.findIndex(sum=>this.partners.length < sum)
-        console.debug(find)
-        const idx = (-1 == find) ? this.outerNums[this.userIconNum.length-1] : find
-        this.outerLen = idx+1
-        console.debug(this.outerLen)
-        this.size = this.userIconSizes[0] + ((this.outerLen < 1) ? 0 : this.userIconSizes.slice(1,this.outerLen+1).reduce((sum,v)=>sum+v)*2)
-        console.debug(this.size)
-        */
         const imgs = Array.from({length: this.outerLen}, (v, k) => k).map(i=>this.#makeOuter(i))
         imgs.push(this.#makeCenter())
         return imgs.join('')
@@ -65,15 +52,12 @@ class CircleSvgGenerator {
         const center = (this.size / 2)
         const size = this.userIconSizes[0]
         const leftTop = center - (size / 2)
-        //return this.#makeUser('https://ytyaru.github.io/', './asset/image/avator.png', size, leftTop, leftTop)
         this.profiles.find(p=>p.address==this.my)
         return this.#makeUser(this.my.url, this.my.avatar, size, leftTop, leftTop)
-        //return this.#makeUser(this.my.profile.url, this.my.profile.avatar, size, leftTop, leftTop)
     }
     #makeOuter(i) { // i=0,1,2,3...
         console.debug(`----- makeOuter ${i} -----`)
         const imgs = []
-        //const itemNum = this.userIconNum[i+1]
         const itemNum = (i == this.outerLen-1) ? this.partners.length - ((i==0) ? 0 : this.outerNums[i-1]) : this.outerNums[i]
         console.debug(itemNum, this.outerLen, this.partners.length)
         const deg = 360 / itemNum
@@ -89,8 +73,6 @@ class CircleSvgGenerator {
         for (let n=0; n<itemNum; n++) {
             const x = Math.cos(start * n - Math.PI / 2) * r + r + leftTop;
             const y = Math.sin(start * n - Math.PI / 2) * r + r + leftTop;
-            //imgs.push(this.#makeUser('', './asset/image/user/kkrn_icon_user_3_resize_min.svg', size, x, y))
-            //imgs.push(this.#makeUser(this.partners[p+i].profile.url, this.partners[p+i].profile.avatar, size, x, y))
             console.debug(n)
             console.debug(this.partners)
             console.debug(this.partners[p+n])
@@ -101,4 +83,17 @@ class CircleSvgGenerator {
         }
         return imgs.join('')
     }
+    /*
+    #calcIconNum(i) { // i:0,1,2,...  return:外周位置におけるアイコン同士が重ならない最大配置数を返す
+        console.debug(i)
+        const middles = this.userIconSizes.slice(1,i+1)
+        // 隣同士が重ならない必要最小限サイズ
+        const outerCircleSize = ((this.userIconSizes[0]+this.userIconSizes[i+1])) + ((0==middles.length) ? 0 : middles.reduce((sum,v)=>sum+v)*2)
+        console.debug(this.userIconSizes.slice(0,i+2))
+        console.debug(outerCircleSize)
+        const circumference = outerCircleSize * Math.PI // 外１全体の円周＝直径×円周率
+        console.debug(circumference)
+        return parseInt(circumference / this.userIconSizes[i+1])
+    }
+    */
 }
